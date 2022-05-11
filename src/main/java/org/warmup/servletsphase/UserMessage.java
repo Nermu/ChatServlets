8fb638efc859de2670f7639e43a2b06b22fbe09b
Nermeen <nermenelsayed33@gmail.com>
2022-05-11 15:18:54 +0200

    first commit

---
+++ src/main/java/org/warmup/servletsphase/UserMessage.java
@@ -0,0 +1,35 @@
+package org.warmup.servletsphase;
+
+
+public class UserMessage {
+    String userID;
+    String message;
+
+    public UserMessage() {
+        userID = "";
+        message = "";
+    }
+    public UserMessage(String userID, String message)
+    {
+        this.userID = userID;
+        this.message = message;
+    }
+
+    public String getUserID()
+    {
+        return userID;
+    }
+    public void setUserID()
+    {
+        this.userID = userID;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+    public void setMessage() {
+        this.message = message;
+    }
+
+}
